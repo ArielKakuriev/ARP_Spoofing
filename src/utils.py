1afc235cbb57a0d5fd913725355de5adf550e921
Ariel-Kakuriev <ariel.kakuriev@gmail.com>
2025-06-30 18:45:14 +0300

    Build a function that gives the MAC address of some IP

---
+++ src/utils.py
@@ -0,0 +1,13 @@
+from scapy.all import srp1, Ether, ARP
+
+BROADCAST_MAC = "ff:ff:ff:ff:ff"
+
+"""
+This function gives the MAC address of some IP
+Input: The IP to find his MAC
+Output: The MAC of the IP 
+"""
+def get_mac_addr(ip: str) -> str:
+    msg = Ether(dst=BROADCAST_MAC) / ARP(pdst=ip)  # ARP packet to get the MAC of the IP
+    res = srp1(msg, verbose=0)
+    return res[ARP].hwsrc  # MAC addr of IP
