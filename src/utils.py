f843d5caab4b8dd1531bb2ca67fc2f3bf4a9987b
Ariel-Kakuriev <ariel.kakuriev@gmail.com>
2025-06-30 21:15:26 +0300

    Build a function that send an ARP packet

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,9 @@
-from scapy.all import srp1, Ether, conf, ARP
+from scapy.all import srp1, Ether, conf, ARP, send
 
 DEFAULT_ROUTE = "0.0.0.0"
 INDEX_OF_IP_ROUTE = 2
 BROADCAST_MAC = "ff:ff:ff:ff:ff"
+DEFAULT_SEND_COUNT = 1
 
 """
 This function gives our mac address
@@ -26,3 +27,11 @@ def get_mac_addr(ip: str) -> str:
     msg = Ether(dst=BROADCAST_MAC) / ARP(pdst=ip)  # ARP packet to get the MAC of the IP
     res = srp1(msg, verbose=0)
     return res[ARP].hwsrc  # MAC addr of IP
+
+"""
+This function send an ARP packet
+Input: The opcode of ARP packet. The MAC & IP of the src. The MAC & IP of the dst. How much times to send the packet(default: 1)
+Output: None
+"""
+def send_arp_packet(opcode: int, src_mac: str, src_ip: str, dst_mac: str, dst_ip: str, times_to_send: int = DEFAULT_SEND_COUNT) -> None:
+    send(ARP(op=opcode, hwsrc=src_mac, psrc=src_ip, hwdst=dst_mac, pdst=dst_ip), count=times_to_send)  # Send an ARP packet
